ce899c167cf97ab1236f138c5631156728a2c31a
Yuku Kotani <yukukotani@gmail.com>
2019-11-20 18:37:12 +0900

    Divide kotlin printer module

--- printer-kotlin/build.gradle.kts
+++ printer-kotlin/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(project(":printer-kotlin"))
+    api("com.github.cretz.kastree:kastree-ast-psi:0.4.0")
+    api(project(":doc"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,11 @@ rootProject.name = "kutie"
 include(
     "cli",
     "doc",
+    "printer-kotlin",
     "example"
 )
 findProject(":cli")?.name = "cli"
 findProject(":doc")?.name = "doc"
+findProject(":printer-kotlin")?.name = "printer-kotlin"
 findProject(":example")?.name = "example"
 
